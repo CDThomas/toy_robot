83695575742d3229f2e126f6adbf93764da7d917
Drew Thomas <cdthomas.92@gmail.com>
2020-02-25 16:00:32 +1100

    Add ToyRobot struct and implement place function

--- lib/toy_robot.ex
+++ lib/toy_robot.ex
@@ -1,18 +1,23 @@
 defmodule ToyRobot do
   @moduledoc """
-  Documentation for ToyRobot.
+  ToyRobot.
   """
+
+  defstruct [:x, :y, :direction]
+
+  @type t :: %__MODULE__{
+          x: 0..4,
+          y: 0..4,
+          direction: direction()
+        }
+
+  @type direction :: :north | :east | :south | :west
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ToyRobot.hello()
-      :world
-
+  Places the toy robot on the table in position x, y and with the direction :north, :south, :east, or :west.
   """
-  def hello do
-    :world
+  @spec place(x :: 0..4, y :: 0..4, direction) :: t()
+  def place(x \\ 0, y \\ 0, direction \\ :north) do
+    %__MODULE__{x: x, y: y, direction: direction}
   end
 end
--- test/toy_robot_test.exs
+++ test/toy_robot_test.exs
@@ -1,8 +1,13 @@
 defmodule ToyRobotTest do
   use ExUnit.Case
-  doctest ToyRobot
 
-  test "greets the world" do
-    assert ToyRobot.hello() == :world
+  describe "place/3" do
+    test "returns a ToyRobot with default values when called with no args" do
+      assert ToyRobot.place() == %ToyRobot{x: 0, y: 0, direction: :north}
+    end
+
+    test "returns a ToyRobot with the correct values when called with args" do
+      assert ToyRobot.place(1, 2, :east) == %ToyRobot{x: 1, y: 2, direction: :east}
+    end
   end
 end
