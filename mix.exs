9d2846c7c6ea7276fd4a0bedbb2e7f7f5a182d95
Drew Thomas <cdthomas.92@gmail.com>
2020-02-25 15:28:03 +1100

    Add dialyxir to deps

--- mix.exs
+++ mix.exs
@@ -21,6 +21,7 @@ defmodule ToyRobot.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:dialyxir, "~> 1.0.0-rc.7", only: [:dev], runtime: false},
       {:mix_test_watch, "~> 1.0", only: :dev, runtime: false}
     ]
   end
