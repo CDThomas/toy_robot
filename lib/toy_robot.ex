e864ffb868b956146cdb7e26db9467162cbe7826
Drew Thomas <cdthomas.92@gmail.com>
2020-02-25 18:57:19 +1100

    Add nil as valid value for x, y, and direction

--- lib/toy_robot.ex
+++ lib/toy_robot.ex
@@ -6,9 +6,9 @@ defmodule ToyRobot do
   defstruct [:x, :y, :direction]
 
   @type t :: %__MODULE__{
-          x: 0..4,
-          y: 0..4,
-          direction: direction()
+          x: 0..4 | nil,
+          y: 0..4 | nil,
+          direction: direction() | nil
         }
 
   @type direction :: :north | :east | :south | :west
@@ -16,7 +16,7 @@ defmodule ToyRobot do
   @doc """
   Places the toy robot on the table in position x, y and with the direction :north, :south, :east, or :west.
   """
-  @spec place(x :: 0..4, y :: 0..4, direction) :: t()
+  @spec place(x :: 0..4, y :: 0..4, direction :: direction()) :: t()
   def place(x \\ 0, y \\ 0, direction \\ :north) do
     %__MODULE__{x: x, y: y, direction: direction}
   end
