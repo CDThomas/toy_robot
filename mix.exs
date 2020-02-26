899562a91249703422b41a38c3b2f57db0c12b1f
Drew Thomas <cdthomas.92@gmail.com>
2020-02-26 17:51:22 +1100

    Add alias for starting HTTP server

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule ToyRobot.MixProject do
       version: "0.1.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      aliases: [http_server: "run --no-halt"]
     ]
   end
 
